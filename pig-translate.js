26f99e1be7628616f517bc7929f29393848967d1
Frank <fr.heggeness@gmail.com>
2019-04-12 09:24:08 -1000

    working translator

---
+++ pig-translate.js
@@ -0,0 +1,19 @@
+'use strict';
+function pigTranslate(string) {
+  let strArray = string.split('');
+  if (['a', 'e', 'i', 'o', 'u', 'y'].includes(string[0])) {
+    let vowelAdd = ['-ay'];
+    strArray.push(vowelAdd);
+    let newString = strArray.join('');
+    return newString;
+  } else {
+    strArray.push('-');
+    while (['a', 'e', 'i', 'o', 'u', 'y'].includes(strArray[0]) === false) {
+      let addtoEnd = strArray.splice(0, 1);
+      strArray.push(addtoEnd);
+    }
+    strArray.push('ay');
+    let newWord = strArray.join('');
+    return newWord;
+  }
+}
