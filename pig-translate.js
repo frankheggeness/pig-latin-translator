9a374e85cf0ee4c76e6ff042afa6f121b0f28ecd
Frank <fr.heggeness@gmail.com>
2019-04-15 09:53:32 -1000

    format

--- pig-translate.js
+++ pig-translate.js
@@ -1,6 +1,7 @@
 'use strict';
 function pigTranslate(string) {
   let strArray = string.split('');
+
   if (['a', 'e', 'i', 'o', 'u', 'y'].includes(string[0])) {
     let vowelAdd = ['-ay'];
     strArray.push(vowelAdd);
@@ -8,12 +9,15 @@ function pigTranslate(string) {
     return newString;
   } else {
     strArray.push('-');
+
     while (['a', 'e', 'i', 'o', 'u', 'y'].includes(strArray[0]) === false) {
       let addtoEnd = strArray.splice(0, 1);
       strArray.push(addtoEnd);
     }
+
     strArray.push('ay');
     let newWord = strArray.join('');
+
     return newWord;
   }
 }
